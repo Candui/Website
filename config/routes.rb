4aaae3461c2d176162d86686ccbadba241ef5b98
Candui <serapup2@mail.ru>
2021-10-16 09:43:29 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/signup'
+  get  '/signup',  to: 'users#new'
+  get '/help', to: 'static_pages#help', as: 'helf'
+  
   resources :microposts
   resources :users
   root 'static_pages#home'
