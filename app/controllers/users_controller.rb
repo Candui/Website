4cfeaa07b0a9fc5e1506568feec3dd083c19aea5
Candui <serapup2@mail.ru>
2021-10-16 12:04:15 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
+   
   end
 
   # GET /users/new
@@ -21,10 +23,14 @@ class UsersController < ApplicationController
 
   # POST /users or /users.json
   def create
+    
     @user = User.new(user_params)
+    
 
     respond_to do |format|
       if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+      
         format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
@@ -64,6 +70,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
     end
 end
